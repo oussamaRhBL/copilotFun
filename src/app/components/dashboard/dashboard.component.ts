963920450a8b2ff9a6bb163b122b68a0b02bb78f
GUEDIRA Nouha <nouha.guedira@berger-levrault.com>
2025-03-12 12:49:56 +0000

    dashboard updated 2

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,23 +1,47 @@
 import { Component } from '@angular/core';
-import {CdkDrag, CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+import {
+  CdkDrag,
+  CdkDragDrop,
+  CdkDropList,
+  CdkDropListGroup,
+  moveItemInArray,
+  transferArrayItem
+} from '@angular/cdk/drag-drop';
 import {MemoComponent, MemoModel} from '../memo/memo.component';
 import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [MemoComponent, CdkDrag, NgForOf],
+  imports: [MemoComponent, CdkDrag, NgForOf, CdkDropList, CdkDropListGroup],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-  memoList: MemoModel[] = [
-    { title: 'Memo 1', content: 'Content 1', date: '2025-03-12' },
-    { title: 'Memo 2', content: 'Content 2', date: '2025-03-13' },
-    { title: 'Memo 3', content: 'Content 3', date: '2025-03-14' }
+
+  todoMemos: MemoModel[]= [
+      { title: 'Memo 1', content: 'Content 1', date: '2025-03-12',color:'#ffb3ba' },
+      { title: 'Memo 2', content: 'Content 2', date: '2025-03-13' ,color:'#ffdfba'}
+    ];
+
+  doneMemos: MemoModel[]= [
+    { title: 'Memo 3', content: 'Content 3', date: '2025-03-14',color:'#ffffba' }
   ];
-  drop(event: CdkDragDrop<MemoModel[]>) {
-    moveItemInArray(this.memoList, event.previousIndex, event.currentIndex);
+  trackByFn(index: number, item: MemoModel): number {
+    return index;
+  }
+
+  drop(event: CdkDragDrop<MemoModel[], any>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
   }
 
 }
--- src/app/components/memo/memo.component.ts
+++ src/app/components/memo/memo.component.ts
@@ -1,22 +1,26 @@
 import {Component, Input} from '@angular/core';
 import {CdkDrag} from '@angular/cdk/drag-drop';
+import {NgStyle} from '@angular/common';
 
 export type MemoModel ={
   title: string;
   content: string;
   date: string;
+  color?: string;
 }
 
 @Component({
   selector: 'app-memo',
   standalone: true,
   imports: [
-    CdkDrag
+    CdkDrag,
+    NgStyle
   ],
   templateUrl: './memo.component.html',
   styleUrl: './memo.component.css'
 })
 export class MemoComponent {
   @Input()
-  memoModel? : MemoModel;
+  memo? : MemoModel;
+
 }
