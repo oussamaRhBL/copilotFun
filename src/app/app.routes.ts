10b1245460aa00378ce27d47671d9c9b21625926
GUEDIRA Nouha <nouha.guedira@berger-levrault.com>
2025-03-12 11:48:37 +0000

    route en cours

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { NoteListComponent } from './pages/note-list/note-list.component';
 import {DashboardComponent} from './components/dashboard/dashboard.component';
+import {MemoComponent} from './components/memo/memo.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'notes', component: NoteListComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'memo', component: MemoComponent },
+
 
 ];
