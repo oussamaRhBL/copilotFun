a37c61023d1d037ba97decf04710b675cd35cc48
Oussama.RHFIR <oussama.rhfir@berger-levrault.com>
2025-03-12 10:46:47 +0000

    adding note list with routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './pages/home/home.component';
+import {NoteListComponent} from './pages/note-list/note-list.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'notes', component: NoteListComponent }
+];
