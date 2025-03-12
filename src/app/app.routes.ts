f5319ad8308384f598440ca0c366ed0ba19471e8
Oussama.RHFIR <oussama.rhfir@berger-levrault.com>
2025-03-12 11:05:12 +0000

    adding component note-app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import {HomeComponent} from './pages/home/home.component';
-import {NoteListComponent} from './pages/note-list/note-list.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NoteListComponent } from './pages/note-list/note-list.component';
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'notes', component: NoteListComponent }
---
+++ src/app/components/note-app/note-app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-note-app',
+  standalone: true,
+  imports: [],
+  templateUrl: './note-app.component.html',
+  styleUrl: './note-app.component.css'
+})
+export class NoteAppComponent {
+
+}
