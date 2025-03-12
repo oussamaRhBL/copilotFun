52091fb2862396cd6b08683871c256f5f21cdbc8
GUEDIRA Nouha <nouha.guedira@berger-levrault.com>
2025-03-12 12:08:38 +0000

    Routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterModule, RouterOutlet} from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { MemoComponent } from './components/memo/memo.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, DashboardComponent, MemoComponent],
+  imports: [RouterModule, RouterOutlet, HomeComponent, DashboardComponent, MemoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 
